bd6649c4ab4e8ed26403970a5df09cd18163e068
cdeler <serj.krotov@gmail.com>
2019-12-06 13:55:10 +0300

    Added readme

---
+++ README.md
@@ -0,0 +1,5 @@
+This package provides type hints for DataBricks utils python binding ([dbutils](https://docs.databricks.com/dev-tools/databricks-utils.html))
+
+The package requires `python >= 3.5`
+
+All contribution are welcome!
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
         name='dbutils_typehint',
-        version='0.0.1',
+        version='0.1.0',
         packages=['dbutils_typehint'],
         url='https://github.com/cdeler/dbutils_typehint',
         license='MIT',
